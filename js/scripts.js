310fef666131cea5d9eea6e644d7db58d18a5112
Chris Carr and Ryan Jones <chris@>
2017-02-20 10:50:17 -0800

    add a multideminsional array and a foreach loop to refactor the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
 $(document).ready(function() {
   $("#submit").click(function(event) {
     event.preventDefault();
-      var color = $("#favColor").val();
-      var number = parseInt($("#favNumber").val());
-      var os = $("#favOS").val();
+      // var color = $("#favColor").val();
+      // var number = parseInt($("#favNumber").val());
+      // var os = $("#favOS").val();
+      //
+      // var array = [color, number, os];
+      // var newArray = [];
+      //
+      // for(i=0; i<newArray.length; i++) {
+      //   $("#results").append("<li>" + multiArray[i]
+      // };
+      //
+      // newArray.push(array[1], array[0], array[2]);
+      // console.log(newArray);
 
-      var array = [color, number, os];
-      var newArray = [];
 
-      newArray.push(array[1], array[0], array[2]);
-      console.log(newArray);
 
-      $("#results").append("<li>Your favorite color is " + array[0] + ".</li>");
-      $("#results").append("<li>Your favorite number is " + array[1] + ".</li>");
-      $("#results").append("<li>Your favorite OS is " + array[2] + ".</li>");
+      // var colorArray = ["#favColor", $("#favColor").val()];
+      // var numberArray = ["#favNumber", $("#favNumber").val()];
+      //
+      // var multiArray = [colorArray, numberArray];
+
+      var multiArray = [["Favorite Color: ", $("#favColor").val(), ".favColor"], ["Favorite Number", $("#favNumber").val(), ".favNumber"], ["Favorite OS: ", $("#favOS").val(), ".favOS"]];
+
+      multiArray.forEach(function(array) {
+        $(array[2]).css({
+          "color": "red",
+          "font-family": "cursive",
+          "font-size": "50px"
+          });
+        $("#results").append("<li>" + array[0] + ": " + array[1] + "</li>");
+      });
+
   });
 });
